89addb797fce75d97295532dc80bbc213d6c4958
Ben Sales <adebsa2401@gmail.com>
2023-02-16 10:53:06 +0100

    feat: create entities: Banner, Campaign, Impression, Site, Visit, Vue and Zone

---
+++ src/Entity/Impression.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImpressionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImpressionRepository::class)]
+class Impression
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'impressions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Vue $vue = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVue(): ?Vue
+    {
+        return $this->vue;
+    }
+
+    public function setVue(?Vue $vue): self
+    {
+        $this->vue = $vue;
+
+        return $this;
+    }
+}
