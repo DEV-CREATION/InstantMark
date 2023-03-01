addb948cb8c46f1367a7608d64555c50928b1cc8
Ben Sales <adebsa2401@gmail.com>
2023-03-01 11:04:06 +0100

    feat: Design dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\BannerRepository;
+use App\Repository\CampaignRepository;
+use App\Repository\ImpressionRepository;
+use App\Repository\SiteRepository;
+use App\Repository\VisitRepository;
+use App\Repository\VueRepository;
+use App\Repository\ZoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +16,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        CampaignRepository  $campaignRepository,
+        BannerRepository $bannerRepository,
+        SiteRepository $siteRepository,
+        ZoneRepository $zoneRepository,
+        VisitRepository $visitRepository,
+        VueRepository $vueRepository,
+        ImpressionRepository $impressionRepository
+    ): Response
     {
-        return $this->redirectToRoute('app_campaign_index');
+        $campaigns = $campaignRepository->findAll();
+        $banners = $bannerRepository->findAll();
+        $sites = $siteRepository->findAll();
+        $zones = $zoneRepository->findAll();
+        $visits = $visitRepository->findAll();
+        $vues = $vueRepository->findAll();
+        $impressions = $impressionRepository->findAll();
+
+        return $this->render("index.html.twig", compact(
+            'campaigns',
+            'banners',
+            'sites',
+            'zones',
+            'visits',
+            'vues',
+            'impressions'
+        ));
     }
 }
