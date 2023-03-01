18e8604fccffac44f3a3ab62f5c14c7238a0b4a9
Ben Sales <adebsa2401@gmail.com>
2023-03-01 12:48:11 +0100

    feat: Implement sites interface

---
+++ src/Form/SiteType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SiteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du site',
+                'attr' => [
+                    'placeholder' => 'Nom du site',
+                ],
+            ])
+            ->add('url', TextType::class, [
+                'label' => 'URL du site',
+                'attr' => [
+                    'placeholder' => 'URL du site',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Site::class,
+        ]);
+    }
+}
